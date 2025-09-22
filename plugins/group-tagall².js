4cb53345b4db924270035e818e5193a0d172a4df
segundocristian219-oss <segundocristian219@gmail.com>
2025-09-22 03:21:24 -0600

    Actualizar group-tagall².js

--- plugins/group-tagall².js
+++ plugins/group-tagall².js
@@ -29,7 +29,7 @@ const handler = async (m, { conn, participants, isAdmin, isOwner }) => {
 
   // Comando .todos
   if (command === 'todos') {
-    const emoji = groupEmojis[chatId] || '🗣️';
+    const emoji = groupEmojis[chatId] || '🌸';
     const total = participants.length;
 
     let mensaje = `*!  MENCION GENERAL  !*\n`;
